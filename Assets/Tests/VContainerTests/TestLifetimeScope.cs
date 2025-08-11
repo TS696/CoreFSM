c08b9b6fb72023a2ab3107ddfd8cd33fd2929dbc
TS696 <takuhanccnxx@gmail.com>
2025-08-11 10:36:08 +0900

    Format

--- Assets/Tests/VContainerTests/TestLifetimeScope.cs
+++ Assets/Tests/VContainerTests/TestLifetimeScope.cs
@@ -7,7 +7,7 @@ namespace Tests.VContainerTests
     public class TestLifetimeScope : LifetimeScope
     {
         public Action<IContainerBuilder> OnConfigure { get; set; }
-        
+
         protected override void Configure(IContainerBuilder builder)
         {
             OnConfigure?.Invoke(builder);
